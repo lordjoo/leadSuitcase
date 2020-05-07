a17d1333b35f5c1d9bc8a057caabcd4829a1b195
Yousef Mahmoud <afifymahmoed@yahoo.com>
2020-05-07 19:31:07 +0200

    =Push missing Setup files

---
+++ setup/service1.js
@@ -0,0 +1,15 @@
+// Copy and paste this into your JavaScript code to initialize the Firebase SDK.
+// You will also need to load the Firebase SDK.
+// See https://firebase.google.com/docs/web/setup for more details.
+
+firebase.initializeApp({
+  "projectId": "lordi-f8663",
+  "appId": "1:415701761297:web:0064cda8dfa665a53da0b0",
+  "databaseURL": "https://lordi-f8663.firebaseio.com",
+  "storageBucket": "lordi-f8663.appspot.com",
+  "locationId": "us-central",
+  "apiKey": "AIzaSyDO9lHA2MKF9mWv24PxLuNATGJi8g9xHdw",
+  "authDomain": "lordi-f8663.firebaseapp.com",
+  "messagingSenderId": "415701761297",
+  "measurementId": "G-XR2QKV2STJ"
+});
